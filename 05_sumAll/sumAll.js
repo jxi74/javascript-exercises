9ff473ac76558d403e856babe9e722c62e471626
jxi74 <112279011+jxi74@users.noreply.github.com>
2024-05-14 17:16:36 -0400

    Add personal solution to 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if (num1 < 0 || Array.isArray(num1) || 
+    typeof num1 === 'string' || num2 < 0 || 
+    Array.isArray(num2) || typeof num2 === 'string') {
+        return 'ERROR';
+    }
 
+    if (num1 < num2) {
+        for (let i = num1; i <= num2; i++) {
+            sum += i;
+        }
+    }
+    else {
+        for (let i = num1; i >= num2; i--) {
+            sum += i;
+        }
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
