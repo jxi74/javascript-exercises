9acff04225e1301d5e2f93d699a6733dfe9a242e
jxi74 <112279011+jxi74@users.noreply.github.com>
2024-05-13 23:23:24 -0400

    Add personal solution to 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let finalString = "";
+    if (num < 0) return "ERROR";
+    for (let i = 0; i < num; i++) {
+        finalString += str;
+    }
 
+    return finalString;
 };
 
 // Do not edit below this line
