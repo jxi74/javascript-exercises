74a2338cde65c198b4bc823cf974d56fa14dec6f
jxi74 <112279011+jxi74@users.noreply.github.com>
2024-05-22 02:39:27 -0400

    Add personal solution to 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    // Selects every character that is not a letter to remove it, 
+    // joins the arrays together, and makes them all lowercase
+    word = word.split(/[^a-zA-z0-9]/).join('').toLowerCase();
+    let reversedWord = "";
+    for (let i = word.length - 1; i >= 0; i--) {
+        reversedWord += word.charAt(i);
+    }
+    return (word === reversedWord ? true : false)
 };
 
 // Do not edit below this line
