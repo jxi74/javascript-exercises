cd6f693a0356704fe9c8742d5302ebb5c882087f
jxi74 <112279011+jxi74@users.noreply.github.com>
2024-05-22 20:31:09 -0400

    Add personal solution to 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
+    let nthValue = helper(Number(num));
+    return nthValue;
 
 };
+
+const helper = function(num) {
+    if (num === 0) {
+        return 0;
+    }
+    if (num === 1) {
+        return 1;
+    }
+
+    return helper(num - 1) + helper(num - 2);
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
